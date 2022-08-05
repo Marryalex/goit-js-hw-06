c14906851e511536aeb2d05ec69a799f6661b0b4
Marry Tymofiichuk <masha432alex@i.ua>
2022-08-05 12:47:02 +0300

    add correct

--- js/task-01.js
+++ js/task-01.js
@@ -2,9 +2,9 @@ const categoryEl = document.querySelectorAll(`.item`);
 const numberCategoryEl = categoryEl.length;
 console.log(`Number of categories:${numberCategoryEl}`);
 
-[...categoryEl].forEach(function (el){
-    const numberOfElements = el.querySelectorAll(`li`);
-    const nameOfElements = el.querySelector(`h2`);
+[...categoryEl].forEach(function (el) {
+    const numberOfElements = el.lastElementChild;
+    const nameOfElements = el.firstElementChild;
     console.log(`Category: ${nameOfElements.textContent}`);
-    console.log(`Elements: ${numberOfElements.length}`)
+    console.log(`Elements: ${numberOfElements.children.length}`)
 })
--- js/task-02.js
+++ js/task-02.js
@@ -7,7 +7,18 @@ const ingredients = [
   'Condiments',
 ];
 const ul = document.querySelector('#ingredients')
-const creatyLiFromArray = (array) =>
-  array.map((el) => `<li class = "item">${el}</li>`).join("");
-  const list = creatyLiFromArray(ingredients)
-ul.insertAdjacentHTML("beforeend", list)
+// const creatyLiFromArray = (array) =>
+//   array.map((el) => `<li class = "item">${el}</li>`).join("");
+//   const list = creatyLiFromArray(ingredients)
+// ul.insertAdjacentHTML("beforeend", list)
+
+
+
+const creatyLiFromArray = ingredients.map((ingredient) => {
+  const list = document.createElement('li');
+  list.classList.add('item');
+  list.textContent = ingredient
+  return list
+}
+)
+ul.append(...creatyLiFromArray)
